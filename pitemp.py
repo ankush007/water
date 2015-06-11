544037a8aeee701ed6d193664f067aece74776c8
Tom Munnecke <munnecke@gmail.com>
2015-06-11 09:36:28 -0700

    Update pitemp.py
    Cleaned up documentation and instructions

--- pitemp.py
+++ pitemp.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 __author__ = 'munnecke'
+# This program logs a Raspberry Pi's CPU temperature to a Thingspeak Channel
+# To use, get a Thingspeak.com account, set up a channel, and capture the Channel Key at https://thingspeak.com/docs/tutorials/ 
+# Then paste your channel ID in the code for the value of "key" below.
+# Then run as sudo python pitemp.py (access to the CPU temp requires sudo access)
+# You can see my channel at https://thingspeak.com/channels/41518
+
 import httplib, urllib
 import time
 sleep = 60 # how many seconds to sleep between posts to the channel
-key = '6KVVK9UGR5P6BD9E'  # Thingspeak channel to update
+key = 'Put your Thingspeak Channel Key here'  # Thingspeak channel to update
 
 #Report Raspberry Pi internal temperature to Thingspeak Channel
 def thermometer():
